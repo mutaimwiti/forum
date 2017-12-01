d89e635799385740468805d7285f9abc45420e31
Mutai Mwiti <mutaimwiti40@gmail.com>
2017-12-01 12:15:54 +0300

    Add ReplyFactory

---
+++ database/factories/ReplyFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+use Faker\Generator as Faker;
+use App\User;
+use App\Thread;
+use App\Reply;
+
+$factory->define(Reply::class, function (Faker $faker) {
+    return [
+        'user_id' => function(){
+            return factory(User::class)->create()->id;
+        },
+        'thread_id' => function(){
+            return factory(Thread::class)->create()->id;
+        },
+        'body' => $faker->paragraph
+    ];
+});
