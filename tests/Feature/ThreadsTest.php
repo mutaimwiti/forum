92d38666c2c43587717365aaf4b1f7b9d0f689fc
Mutai Mwiti <mutaimwiti40@gmail.com>
2017-12-01 13:28:56 +0300

    Add basic tests

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -3,19 +3,29 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class ExampleTest extends TestCase
+class ThreadsTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+    use DatabaseMigrations;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads');
+
+        $response->assertSee($thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/' . $thread->id);
+
+        $response->assertSee($thread->title);
     }
 }
