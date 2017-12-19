0660e752633c9c4e5a74f22e9c7f909140fcb758
Mutai Mwiti <mutaimwiti40@gmail.com>
2017-12-19 04:44:41 +0300

    Rename ThreadsTest to ReadThreadsTest and move creation of thread to setup method

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -5,27 +5,30 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class ThreadsTest extends TestCase
+class ReadThreadsTest extends TestCase
 {
     use DatabaseMigrations;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
 
     /** @test */
     public function a_user_can_view_all_threads()
     {
-        $thread = factory('App\Thread')->create();
-
         $response = $this->get('/threads');
 
-        $response->assertSee($thread->title);
+        $response->assertSee($this->thread->title);
     }
 
     /** @test */
     public function a_user_can_read_a_single_thread()
     {
-        $thread = factory('App\Thread')->create();
+        $response = $this->get('/threads/' . $this->thread->id);
 
-        $response = $this->get('/threads/' . $thread->id);
-
-        $response->assertSee($thread->title);
+        $response->assertSee($this->thread->title);
     }
 }
