86096ccfe418997c61ab6080d2373d48a8d35ea8
Mutai Mwiti <mutaimwiti40@gmail.com>
2017-12-19 05:59:10 +0300

    Cleanup thread unit test

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -10,17 +10,22 @@ class ThreadTest extends TestCase
 {
     use DatabaseMigrations;
 
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory(Thread::class)->create();
+    }
+
     /** @test  */
     public function a_thread_has_replies()
     {
-        $thread = factory(Thread::class)->create();
-        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $thread->replies);
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
     }
 
     /** @test */
     public function a_thread_has_a_creator()
     {
-        $thread = factory(Thread::class)->create();
-        $this->assertInstanceOf('App\User', $thread->creator);
+        $this->assertInstanceOf('App\User', $this->thread->creator);
     }
 }
