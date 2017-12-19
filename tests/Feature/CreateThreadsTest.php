71073367d9fe17323f36f07257afa67733b841ea
Mutai Mwiti <mutaimwiti40@gmail.com>
2017-12-19 12:00:03 +0300

    Setup create thread logic and test to check that only authenticted users can create thread

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Thread;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class CreateThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function an_authenticated_user_can_create_new_forum_threads(){
+        $this->actingAs(factory('App\User')->create());
+
+        $thread = factory(Thread::class)->make();
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+}
