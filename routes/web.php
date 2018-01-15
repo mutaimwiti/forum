1d9bcaa3856c896e159fa5a94c4a8e67ec30f761
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-01-15 10:47:15 +0300

    Enable deletion of threads

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::get('/threads/create', 'ThreadController@create');
 
 Route::get('/threads/{channel}/{thread}', 'ThreadController@show');
 
+Route::delete('/threads/{channel}/{thread}', 'ThreadController@destroy');
+
 Route::post('/threads', 'ThreadController@store');
 
 Route::get('/threads/{channel}', 'ThreadController@index');
