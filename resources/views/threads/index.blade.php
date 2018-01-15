f72f2362067f41c21578cd16826fac04a4001ac5
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-01-15 10:52:20 +0300

    Inform user when there are no threads for a channel

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                @foreach($threads as $thread)
+                @forelse($threads as $thread)
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <div class="level">
@@ -23,7 +23,11 @@
                             <div class="body">{{ $thread->body }}</div>
                         </div>
                     </div>
-                @endforeach
+                @empty
+                    <div class="alert alert-info">
+                        <p>There are no relevant results at this time.</p>
+                    </div>
+                @endforelse
             </div>
         </div>
     </div>
