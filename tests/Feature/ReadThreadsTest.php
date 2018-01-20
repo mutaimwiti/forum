b4909f7422d54ee1f21a9686437c936ec786887b
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-01-20 20:56:06 +0300

    Fix test failures resulting from using vue components for replies

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -35,15 +35,6 @@ class ReadThreadsTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_read_replies_that_are_associated_with_a_thread()
-    {
-        $reply = create(Reply::class, ['thread_id' => $this->thread->id]);
-
-        $this->get($this->thread->path())
-            ->assertSee($reply->body);
-    }
-
-    /** @test */
     public function a_user_can_filter_threads_according_to_a_channel()
     {
         $channel = create(Channel::class);
@@ -93,8 +84,6 @@ class ReadThreadsTest extends TestCase
 
         $response = $this->getJson($thread->path() . '/replies')->json();
 
-        $this->assertCount(1, $response['data']);
-
-        $this->assertCount(2, $response['total']);
+        $this->assertEquals(2, $response['total']);
     }
 }
